c4cc8885d4886b8f6c138fb7548d53bd2f2e6e38
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-15 18:56:16 -0300

    refactor: optimize get_tasks using joinedload

--- routers/tasks.py
+++ routers/tasks.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session, selectinload
+from sqlalchemy.orm import Session, joinedload
 from typing import List
 from schemas.tasks import TaskResponse
 from database import get_db
@@ -9,4 +9,4 @@ router = APIRouter()
 
 @router.get('/tasks', response_model=List[TaskResponse])
 def get_tasks(db: Session = Depends(get_db)):
-    return db.query(Task).options(selectinload(Task.user)).all()
+    return db.query(Task).options(joinedload(Task.user)).all()
