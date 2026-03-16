e16b39baacff6e4a3c2997882bd5dbf25259f48e
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-16 20:09:26 -0300

    feat: add endpoint to get task by id

--- routers/tasks.py
+++ routers/tasks.py
@@ -10,3 +10,7 @@ router = APIRouter()
 @router.get('/tasks', response_model=List[TaskResponse])
 def get_tasks(db: Session = Depends(get_db)):
     return db.query(Task).options(joinedload(Task.user)).all()
+
+@router.get('/tasks/{task_id}', response_model=TaskResponse)
+def get_task(task_id: int, db: Session = Depends(get_db)):
+    return db.query(Task).filter_by(id = task_id).all()
