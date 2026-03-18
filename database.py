a68d03eae1081e3c591901ece057082c11fb2666
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-18 20:41:41 -0300

    feat: add declarative base to enable ORM mapping

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 conexao = 'mysql+pymysql://root:12345r@localhost:3306/user_management_api'
+
 engine = create_engine(conexao)
+
 Session = sessionmaker(bind = engine)
+
+Base = declarative_base()
 
 def get_db():
     db = Session()
