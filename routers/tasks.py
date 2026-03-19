95cd1d267aba7f86247398a3d4786a33bad3e97e
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-19 20:46:14 -0300

    fix: correct query in get_user_with_task endpoint

--- routers/tasks.py
+++ routers/tasks.py
@@ -25,7 +25,7 @@ def get_tasks(db: Session = Depends(get_db)):
     
 @router.get('/tasks/tasks-with-user', response_model=List[TaskResponse])
 def get_tasks_with_user(db: Session = Depends(get_db)):
-    return db.query(Task).options(joinedload(Task.user)).all()
+    return db.query(Task).filter(Task.user_id != None).options(joinedload(Task.user)).all()
 
 @router.get('tasks/tasks-without-user', response_model=List[TaskResponse])
 def get_tasks_without_user(db: Session = Depends(get_db)):
