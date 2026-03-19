972cb73f1928fe9e0ec5040219b341e71ddaead5
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-19 20:41:19 -0300

    feat: add create task endpoint

--- routers/tasks.py
+++ routers/tasks.py
@@ -1,11 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session, joinedload
 from typing import List
-from schemas.tasks import TaskResponse, TaskUpdate
+from schemas.tasks import TaskResponse, TaskUpdate, TaskCreate
 from database import get_db
 from models.tasks import Task
 
 router = APIRouter()
+
+@router.post('/tasks', status_code=201, response_model=TaskResponse)
+def create_task(task: TaskCreate, db: Session = Depends(get_db)):
+    new_task = Task(title = task.title, description = task.description)
+    
+    db.add(new_task)
+    
+    db.commit()
+    
+    db.refresh(new_task)
+    
+    return new_task
 
 @router.get('/tasks', response_model=List[TaskResponse])
 def get_tasks(db: Session = Depends(get_db)):
