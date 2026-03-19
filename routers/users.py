e4b0504dea649809ddcc0112c02859e362e0fb2e
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-19 06:04:52 -0300

    fix: resolve route conflicts

--- routers/users.py
+++ routers/users.py
@@ -23,33 +23,24 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh()
     
     return new_user
-
-@router.put('/users/{id_user}', status_code=200, response_model=UserResponse)
-def update_user(id_user: int, user_updated: UserUpdate, db: Session = Depends(get_db)):
-    get_user = db.query(User).filter_by(id = id_user).first()
-    
-    if not get_user:
-        raise HTTPException(status_code=404, detail='Id not exists!')
-    
-    email_existing = db.query(User).filter_by(email = user_updated.email).first()
-    
-    if email_existing and email_existing.id != id_user:
-        raise HTTPException(status_code=409, detail='Email already registered!')
-    
-    get_user.name = user_updated.name
-    get_user.email = user_updated.email
-    
-    db.commit()
-    
-    db.refresh(get_user)
-    
-    return get_user
-    
     
 @router.get('/users', status_code=200, response_model=List[UserResponse])
 def get_users(db: Session = Depends(get_db)):
     return db.query(User).all()
     
+@router.get('/users/users-with-tasks', status_code=200, response_model=List[UserWithTasksResponse])
+def get_users_with_tasks(db: Session = Depends(get_db)):
+    return db.query(User).filter(User.tasks.any()).options(selectinload(User.tasks)).all()
+
+@router.get('/users/{user_id}/tasks', response_model=UserWithTasksResponse)
+def get_user_tasks(user_id: int, db: Session = Depends(get_db)):
+    user_tasks = db.query(User).filter_by(id = user_id).options(joinedload(User.tasks)).first()
+    
+    if not user_tasks:
+        raise HTTPException(status_code=404, detail='Id not exists!')
+    
+    return user_tasks
+
 @router.get('/users/{id_user}', status_code=200, response_model=UserResponse)
 def get_user(id_user: int, db: Session = Depends(get_db)):
     user_exists = db.query(User).filter_by(id == id_user).first()
@@ -70,15 +61,23 @@ def delete_user(id_user: int, db: Session = Depends(get_db)):
     
     db.commit()
     
-@router.get('/users/users-with-tasks', status_code=200, response_model=List[UserWithTasksResponse])
-def get_users_with_tasks(db: Session = Depends(get_db)):
-    return db.query(User).filter(User.tasks.any()).options(selectinload(User.tasks)).all()
-
-@router.get('/users/{user_id}/tasks', response_model=UserWithTasksResponse)
-def get_user_tasks(user_id: int, db: Session = Depends(get_db)):
-    user_tasks = db.query(User).filter_by(id = user_id).options(joinedload(User.tasks)).first()
+@router.put('/users/{id_user}', status_code=200, response_model=UserResponse)
+def update_user(id_user: int, user_updated: UserUpdate, db: Session = Depends(get_db)):
+    get_user = db.query(User).filter_by(id = id_user).first()
     
-    if not user_tasks:
+    if not get_user:
         raise HTTPException(status_code=404, detail='Id not exists!')
     
-    return user_tasks
+    email_existing = db.query(User).filter_by(email = user_updated.email).first()
+    
+    if email_existing and email_existing.id != id_user:
+        raise HTTPException(status_code=409, detail='Email already registered!')
+    
+    get_user.name = user_updated.name
+    get_user.email = user_updated.email
+    
+    db.commit()
+    
+    db.refresh(get_user)
+    
+    return get_user
