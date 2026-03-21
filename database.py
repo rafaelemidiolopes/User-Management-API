3b3671f3eba8898b2ec9bb3d9bc31ac776879685
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-21 16:45:29 -0300

    fix: import Base from models

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from models.base import Base
 
 conexao = 'mysql+pymysql://root:12345r@localhost:3306/user_management_api'
 
@@ -7,7 +8,7 @@ engine = create_engine(conexao)
 
 Session = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
-Base = declarative_base()
+Base = Base
 
 def get_db():
     db = Session()
