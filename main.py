69128b490419b94d01f4a260143b64846dfdd1a9
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-21 16:38:49 -0300

    refactor: fix router naming conflicts

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from database import engine, Base
 from fastapi import FastAPI
-from routers import users, tasks
+from routers.users import router as users_router
+from routers.tasks import router as tasks_router
+from models.users import User
+from models.tasks import Task
 
 app = FastAPI()
 
-Base.metadata.create_all(engine)
+Base.metadata.create_all(bind=engine)
 
-app.include_router(users.router)
-app.include_router(tasks.router)
+app.include_router(users_router)
+app.include_router(tasks_router)
