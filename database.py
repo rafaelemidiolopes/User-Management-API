1d5af2a885e7e266c902956e03660dfa7d473ca8
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-22 16:15:42 -0300

    refactor: remove redundant Base

--- database.py
+++ database.py
@@ -1,14 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models.base import Base
 
 DATABASE_URL = 'mysql+pymysql://root:12345r@localhost:3306/user_management_api'
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
-
-Base = Base
 
 def get_db():
     db = SessionLocal()
