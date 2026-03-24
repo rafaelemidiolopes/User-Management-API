495702ba1aa03aec580134aefcb7625d66d87707
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-24 20:28:58 -0300

    refactor: simplify user_id validation in create_task service

--- services/tasks.py
+++ services/tasks.py
@@ -5,11 +5,8 @@ from sqlalchemy.orm import joinedload
 
 def create_task(task, db):    
     new_task = Task(title = task.title, description = task.description, user_id = task.user_id)
-    
-    if new_task.user_id == 0:
-        new_task.user_id = None
-    
-    if new_task.user_id != None:
+
+    if new_task.user_id is not None:
         user_exists = db.query(User).filter_by(id = task.user_id).first()
     
         if not user_exists:
