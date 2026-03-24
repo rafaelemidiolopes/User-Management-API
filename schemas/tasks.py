88835c4b68eb9f4c55bac9433144c7f7fcb119f8
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-24 05:57:44 -0300

    fix: correct syntax in TaskBase and TaskUpdate schemas

--- schemas/tasks.py
+++ schemas/tasks.py
@@ -5,16 +5,16 @@ class TaskBase(BaseModel):
     title: str = Field(min_length=3)
     description: str | None
     status: TaskStatus = TaskStatus.pending
-    user_id: int = None
+    user_id: int | None = None
     
 class TaskCreate(TaskBase):
     pass
 
 class TaskUpdate(BaseModel):
     title: str | None = Field(None, min_length = 3) 
-    description: str = None
-    status: TaskStatus = None
-    user_id: int = None
+    description: str | None = None
+    status: TaskStatus | None = None
+    user_id: int | None = None
     
 class TaskResponse(BaseModel):
     id: int
