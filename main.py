c7c596595da267f704fca414d195b11591891879
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-26 20:38:25 -0300

    refactor: import Base from models.base instead of database

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from database import engine, Base
+from database import engine
 from fastapi import FastAPI
 from routers.users import router as users_router
 from routers.tasks import router as tasks_router
 from models.users import User
 from models.tasks import Task
+from models.base import Base
 
 app = FastAPI()
 
