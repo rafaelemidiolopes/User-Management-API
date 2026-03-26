d60c35dd762dbb07b7dbadf20d4d36f9ea4a701c
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-25 21:11:49 -0300

    refactor: add type hints and standardize parameter order in task services

--- services/tasks.py
+++ services/tasks.py
@@ -1,9 +1,10 @@
 from models.tasks import Task
 from models.users import User
 from fastapi import HTTPException
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, Session
+from schemas.tasks import TaskUpdate, TaskCreate
 
-def create_task(task, db):    
+def create_task(task: TaskCreate, db: Session):    
     new_task = Task(title = task.title, description = task.description, status = task.status, user_id = task.user_id)
 
     if new_task.user_id is not None:
@@ -20,17 +21,17 @@ def create_task(task, db):
     
     return new_task
 
-def get_tasks(db):
+def get_tasks(db: Session):
     return db.query(Task).options(joinedload(Task.user)).all()
 
-def get_tasks_with_user(db):
+def get_tasks_with_user(db: Session):
     return db.query(Task).filter(Task.user_id.isnot(None)).options(joinedload(Task.user)).all()
 
-def get_tasks_without_user(db):
+def get_tasks_without_user(db: Session):
     return db.query(Task).filter(Task.user_id.is_(None)).all()
 
-def update_task(task_id, task_updated, db):
-    task = get_task_or_404(db, task_id)
+def update_task(task_id: int, task_updated: TaskUpdate, db: Session):
+    task = get_task_or_404(task_id, db)
     
     if task_updated.user_id is not None:
         user = db.query(User).filter_by(id = task_updated.user_id).first()
@@ -49,13 +50,13 @@ def update_task(task_id, task_updated, db):
     
     return task
 
-def delete_task(task_id, db):
-    task = get_task_or_404(db, task_id)
+def delete_task(task_id: int, db: Session):
+    task = get_task_or_404(task_id, db)
         
     db.delete(task)
     db.commit()
     
-def get_task_or_404(db, task_id):
+def get_task_or_404(task_id: int, db: Session):
     task = db.query(Task).filter_by(id = task_id).first()
     
     if not task:
