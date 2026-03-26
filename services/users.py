bc1e9198531f4e515885ab6c8af8183169b6c3ea
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-26 06:41:03 -0300

    refactor: standardize parameter order and names in user services

--- services/users.py
+++ services/users.py
@@ -3,9 +3,9 @@ from models.users import User
 from sqlalchemy.orm import selectinload, joinedload
 
 def create_user(user, db):
-    user_existing = db.query(User).filter_by(email = user.email).first()
+    email_existing = db.query(User).filter_by(email = user.email).first()
     
-    if user_existing:
+    if email_existing:
         raise HTTPException(status_code=409, detail='Email already registered!')
     
     new_user = User(name = user.name, email = user.email)
@@ -25,38 +25,38 @@ def get_users_with_tasks(db):
     return db.query(User).filter(User.tasks.any()).options(selectinload(User.tasks)).all()
 
 def get_user_tasks(user_id, db):
-    user_tasks = db.query(User).filter_by(id = user_id).options(joinedload(User.tasks)).first()
+    user_with_tasks = db.query(User).filter_by(id = user_id).options(joinedload(User.tasks)).first()
     
-    if not user_tasks:
+    if not user_with_tasks:
         raise HTTPException(status_code=404, detail='Id not exists!')
     
-    return user_tasks
+    return user_with_tasks
 
-def delete_user(id_user, db):
-    user = get_user_or_404(id_user, db)
+def delete_user(user_id, db):
+    user = get_user_or_404(user_id, db)
     
-    db.query(User).filter_by(id = id_user).delete()
+    db.query(User).filter_by(id = user_id).delete()
     
     db.commit()
     
-def update_user(id_user, user_updated, db):
-    get_user = get_user_or_404(id_user, db)
+def update_user(user_id, user_updated, db):
+    user = get_user_or_404(user_id, db)
     
     email_existing = db.query(User).filter_by(email = user_updated.email).first()
     
-    if email_existing and email_existing.id != id_user:
+    if email_existing and email_existing.id != user_id:
         raise HTTPException(status_code=409, detail='Email already registered!')
     
     dict_user_updated = user_updated.model_dump(exclude_unset = True)
     
     for field, value in dict_user_updated.items():
-        setattr(get_user, field, value)
+        setattr(user, field, value)
     
     db.commit()
     
-    db.refresh(get_user)
+    db.refresh(user)
     
-    return get_user
+    return user
 
 def get_user_or_404(user_id, db):
     user = db.query(User).filter_by(id = user_id).first()
