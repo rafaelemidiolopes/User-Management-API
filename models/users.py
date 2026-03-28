97fd767b1b4461187ee6a7fff3cff653389b4092
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-28 18:19:35 -0300

    feat: add password_hash field in User model

--- models/users.py
+++ models/users.py
@@ -9,5 +9,6 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key = True)
     name: Mapped[str] = mapped_column(String(40))
     email: Mapped[str] = mapped_column(String(60), unique = True)
+    password_hash: Mapped[str] = mapped_column(String(255))
     
     tasks: Mapped[list['Task']] = relationship(back_populates = 'user')
