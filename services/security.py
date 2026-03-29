9dc336dd1695675a71b2a328e646affc6cdf600f
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-03-29 17:50:06 -0300

    feat: add create_token function in security services

--- services/security.py
+++ services/security.py
@@ -1,9 +1,26 @@
 from pwdlib import PasswordHash
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+from jwt import encode
 
 hasher = PasswordHash.recommended()
+
+SECRET_KEY = '12345'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def get_password_hash(password: str):
     return hasher.hash(password)
 
 def verify_password(password: str, hash_password: str):
     return hasher.verify(password, hash_password)
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    
+    expire = datetime.now(tz= ZoneInfo('UTC')) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expire})
+    
+    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm = ALGORITHM)
+    
+    return encoded_jwt
